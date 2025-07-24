90c461d0e1bcf423a088e5cfa73c60792325a69f
DaMarine <pierslwilliams@gmail.com>
2025-07-24 09:26:28 +0100

    use utc

--- slabcli/pull.py
+++ slabcli/pull.py
@@ -23,12 +23,12 @@ def run(args):
     last_pull_config_only = cfg.get("meta", {}).get("last_pull_cfg_only")
 
     if last_pull_all_files:
-        ts_local = datetime.fromtimestamp(last_pull_all_files, tz=timezone.utc).astimezone()
-        ts_readable = ts_local.strftime("%Y-%m-%d %H:%M:%S %Z")
+        ts_local = datetime.fromtimestamp(last_pull_all_files, tz=timezone.utc)
+        ts_readable = ts_local.strftime("%Y-%m-%d %H:%M:%S UTC")
         print(clicolors.OKGREEN + f"Last pull of all files/folders from Production to Staging occurred at: {ts_readable}")
     if last_pull_config_only:
-        ts_local = datetime.fromtimestamp(last_pull_config_only, tz=timezone.utc).astimezone()
-        ts_readable = ts_local.strftime("%Y-%m-%d %H:%M:%S %Z")
+        ts_local = datetime.fromtimestamp(last_pull_config_only, tz=timezone.utc)
+        ts_readable = ts_local.strftime("%Y-%m-%d %H:%M:%S UTC")
         print(clicolors.OKCYAN + f"Last pull with --update-only from Production to Staging occurred at: {ts_readable}")
     print('')
 
