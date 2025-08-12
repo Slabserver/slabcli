fb0e63243b6046c43209c9a58fb164812aecbf30
DaMarine <pierslwilliams@gmail.com>
2025-08-13 00:42:05 +0100

    shorten debug wording for push types

--- slabcli/common/sync.py
+++ slabcli/common/sync.py
@@ -61,9 +61,9 @@ def run(args, cfg):
         print(clifmt.LIGHT_GRAY + "source_servers =", source_servers)
         print(clifmt.LIGHT_GRAY + "dest_servers =", dest_servers)
         print(clifmt.LIGHT_GRAY + "exempt paths =", exempt_paths)
-        print(clifmt.LIGHT_GRAY + "allowed prod sync paths =", push_paths) 
-        print(clifmt.LIGHT_GRAY + "allowed prod sync files =", push_files) 
-        print(clifmt.LIGHT_GRAY + "allowed prod sync filetypes =", push_filetypes) 
+        print(clifmt.LIGHT_GRAY + "allowed push paths =", push_paths) 
+        print(clifmt.LIGHT_GRAY + "allowed push files =", push_files) 
+        print(clifmt.LIGHT_GRAY + "allowed push filetypes =", push_filetypes) 
         print("")
 
         return
